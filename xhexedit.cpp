1f6d64da3c6d8e60f3b920cde0ceccc28f66b7c7
horsicq <horsicq@gmail.com>
2022-11-08 12:53:01 +0100

    Fix: 2022-11-08

--- xhexedit.cpp
+++ xhexedit.cpp
@@ -24,7 +24,7 @@ XHexEdit::XHexEdit(QWidget *pParent) : XDeviceTableView(pParent)
 {
     g_nBytesProLine=16; // TODO Set/Get !!!
     g_nAddressWidth=8; // TODO Set/Get !!!
-    g_nCursorHeight=2; // TODO Set/Get
+    g_nCursorHeight=2; // TODO Set/Get !!!
     g_nDataBlockSize=0;
     g_nStartOffset=0;
     g_bIsAddressColon=false;
